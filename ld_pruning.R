ebef79ba3c9eea11203974e93871ff7ac5c71285
Stephanie M. Gogarten <sdmorris@uw.edu>
2020-06-12 15:46:32 -0700

    use pipes for arguments

--- ld_pruning.R
+++ ld_pruning.R
@@ -1,17 +1,19 @@
 #! /usr/bin/env Rscript
 library(argparser)
+library(magrittr)
 
 # read arguments
-argp <- arg_parser("LD pruning")
-argp <- add_argument(argp, "gds_file", help="GDS file")
-argp <- add_argument(argp, "--out_file", help="output file name", default="pruned_snps.rds")
-argp <- add_argument(argp, "--sample_id", help="RDS file with vector of sample.id to include")
-argp <- add_argument(argp, "--variant_id", help="RDS file with vector of variant.id to include")
-argp <- add_argument(argp, "--maf", help="minimum MAF for variants to include", default=0.05)
-argp <- add_argument(argp, "--missing", help="maximum missing call rate for variants to include", default=0.05)
-argp <- add_argument(argp, "--r_threshold", help="r threshold for LD", default=sqrt(0.1))
-argp <- add_argument(argp, "--window_size", help="window size in Mb", default=10)
+argp <- arg_parser("LD pruning") %>%
+    add_argument("gds_file", help="GDS file") %>%
+    add_argument("--out_file", help="output file name", default="pruned_snps.rds") %>%
+    add_argument("--sample_id", help="RDS file with vector of sample.id to include") %>%
+    add_argument("--variant_id", help="RDS file with vector of variant.id to include") %>%
+    add_argument("--maf", help="minimum MAF for variants to include", default=0.05) %>%
+    add_argument("--missing", help="maximum missing call rate for variants to include", default=0.05) %>%
+    add_argument("--r_threshold", help="r threshold for LD", default=sqrt(0.1)) %>%
+    add_argument("--window_size", help="window size in Mb", default=10)
 argv <- parse_args(argp)
+
 library(SeqArray)
 library(SNPRelate)
 sessionInfo()
