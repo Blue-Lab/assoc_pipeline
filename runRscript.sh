802fe2fd8dfb78bbb5a2c211cb20aa6ed75616b0
Stephanie Gogarten <sdmorris@uw.edu>
2020-07-17 17:48:39 -0700

    add bash script to submit R scripts to PBS

---
+++ runRscript.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+### combine PBS standard output and error files
+#PBS -j oe
+#PBS -o $PBS_JOBNAME.o$PBS_JOBID
+
+### change to working directory where you submit job 
+cd $PBS_O_WORKDIR
+
+### use PBS array id to specify chromosome
+if [ "$PBS_ARRAYID" == "undefined" ] || [ "$PBS_ARRAYID" == "" ]; then
+    TASK=""
+else
+    TASK="--chromosome $PBS_ARRAYID"
+fi
+
+#args=("$@") # all arguments
+#unset args[0] # remove first argument (R script name)
+
+R -q --vanilla --args ${args} $TASK < ${R}
