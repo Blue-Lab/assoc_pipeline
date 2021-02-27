f5335c863a3dc843da89490b9b7b44b6ac10f3ed
Stephanie M. Gogarten <sdmorris@uw.edu>
2021-02-26 21:19:44 -0800

    add chromosome to output filename

--- coxmeg.R
+++ coxmeg.R
@@ -3,23 +3,20 @@ library(argparser)
 library(magrittr)
 argp <- arg_parser("coxmeg") %>%
     add_argument("--gds_file", help = "GDS file") %>%
-    add_argument("--pheno_file",
-                 help = "Phenotype file in annotated dataframe format") %>%
+    add_argument("--pheno_file", help = "Phenotype file in annotated dataframe format") %>%
     add_argument("--grm_file", help = "GRM file") %>%
     add_argument("--grm_type", help = "matrix type: 'bd' (block diagonal), 'sparse', or 'dense'", default = 'bd') %>%
     add_argument("--grm_spd", help = "is matrix SPD?", flag = TRUE) %>%
     add_argument("--time", help = "time variable name") %>%
     add_argument("--status", help = "status variable name") %>%
-    add_argument("--out_file", help="output file name",
-                 default = "coxmeg.rds") %>%
-    add_argument("--covars",
-                 help = "Covariate variable names (space-separated)", nargs = Inf) %>%
+    add_argument("--out_prefix", help = "Prefix for output files", default = "") %>%
+    add_argument("--covars", help = "Covariate variable names (space-separated)", nargs = Inf) %>%
     add_argument("--sample_id", help = "File with vector of sample IDs") %>%
     add_argument("--variant_id", help = "File with vector of variant IDs") %>%
     add_argument("--maf", help = "minimum MAF of variants to test", default = 0.01) %>%
     add_argument("--score", help = "perform score test", flag = TRUE) %>%
     add_argument("--threshold", help = "reestimate HRs for variants with a p-value<threshold by first estimating a variant-specific variance component", default = 0) %>%
-    add_argument("--chromosome", help = "chromosome number") %>%
+    add_argument("--chromosome", help = "chromosome number")
 
 argv <- parse_args(argp)
 
@@ -68,6 +65,11 @@ res <- coxmeg_gds(gds=gds, pheno=pheno, corr=grm, type=argv$grm_type,
                   cov=cov, maf=argv$maf, score=argv$score,
                   threshold=argv$threshold, spd=argv$grm_spd)
 
-saveRDS(res, file=argv$out_file)
+if (!is.na(argv$chromosome)) {
+    out_file <- paste0(argv$out_prefix, "coxmeg_chr", argv$chromosome, ".rds")
+} else {
+    out_file <- paste0(argv$out_prefix, "coxmeg.rds")
+}
+saveRDS(res, file=$out_file)
 
 seqClose(gds)
