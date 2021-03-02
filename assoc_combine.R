370cf4dd693b7d212974415c6ca56181ed702be6
Stephanie M. Gogarten <sdmorris@uw.edu>
2021-03-02 15:27:04 -0800

    add script to combine per-chrom assoc result files

---
+++ assoc_combine.R
@@ -0,0 +1,28 @@
+#! /usr/bin/env Rscript
+library(argparser)
+
+argp <- arg_parser("Association plots") %>%
+argp <- add_argument("assoc_prefix",
+                     help = "Prefix for association test results file (_chr*.rds)") %>%
+    add_argument("--delete_indiv", help = "delete individual chromosome files", flag = TRUE)
+argv <- parse_args(argp)
+
+sessionInfo()
+print(argv)
+library(dplyr)
+
+## find all files and order by chromosome
+file.pattern <- paste0(basename(argv$assoc_prefix), "assoc_chr[[:alnum:]]+.rds")
+files <- list.files(path=dirname(argv$assoc_prefix), pattern=file.pattern, full.names=TRUE)
+chrs <- sub("_chr", "", regmatches(basename(files), regexpr("_chr[[:alnum:]]+", basename(files))))
+chrs <- factor(chrs, levels=c(0:26,"X","XY","Y"))
+files <- files[order(chrs)]
+
+assoc <- lapply(files, readRDS) %>%
+    bind_rows()
+
+outfile <- paste0(argv$assoc_prefix, "assoc.rds")
+saveRDS(assoc, file=outfile)
+
+## delete individual files
+if (argv$delete_indiv) unlink(files)
