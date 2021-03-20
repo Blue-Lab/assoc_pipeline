b6de1dc21a1d89669aec053cb63f8ad3d72b0d3b
Jai Broome <jb@jaibroome.com>
2021-03-19 17:15:31 -0700

    RUTH: add pipeline script to format PC-AiR pcs in a RUTH-compatible way

---
+++ ruth_format_pcs.R
@@ -0,0 +1,30 @@
+#! /usr/bin/env Rscript
+library(argparser)
+library(magrittr)
+
+argp <- arg_parser("Format PC-AiR PCs for RUTH") %>%
+  add_argument("pcair_file", help = "File with pcair object (.rds)") %>%
+  add_argument("fam_file", help = "PLINK .fam file. Can be partial version from get_controls.R") %>%
+  add_argument("--out_file", help = "Name for output file",
+               default = "ruth_pcs.txt")
+argv <- parse_args(argp)
+
+sessionInfo()
+print(argv)
+
+library(dplyr)
+
+fam <- read.table(argv$fam_file)[,1:2] %>%
+  set_colnames(c("FID", "SAMPLE_ID"))
+
+pcair <- readRDS(argv$pcair_file)
+pcs <- as.data.frame(pcair$vectors) %>%
+  # variable names must be SAMPLE_ID, PC1, PC2, ....
+  rename_with(~ gsub("V", "PC", .x)) %>%
+  mutate(SAMPLE_ID = rownames(.)) %>%
+  left_join(fam, by = "SAMPLE_ID") %>%
+  # RUTH requires the ID field to be a concatenation of FID and IID
+  mutate(SAMPLE_ID = paste(FID, SAMPLE_ID, sep = "_")) %>%
+  select(SAMPLE_ID, matches("PC"))
+write.table(pcs, argv$out_file, sep = "\t", na = "", row.names = FALSE,
+            quote = FALSE)
