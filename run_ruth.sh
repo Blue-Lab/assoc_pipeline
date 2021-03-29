2f50fe613e9b3d40c67155b1990005d3386a159f
Jai Broome <jb@jaibroome.com>
2021-03-29 16:05:52 -0700

    Add RUTH wrapper script

---
+++ run_ruth.sh
@@ -0,0 +1,65 @@
+#! /usr/bin/env bash
+
+# Set default args
+FIELD=GT
+
+while getopts ":p:d:P:o:g:D:k:v:s:n:f:" opt; do
+  case ${opt} in
+    p)
+      PHENO_FILE=$OPTARG
+      ;;
+    d)
+      DX=$OPTARG
+      ;;
+    P)
+      PLINK_PREF=$OPTARG
+      ;;
+    o)
+      OUT_PREF=$OPTARG
+      ;;
+    g)
+      GDS_FILE=$OPTARG
+      ;;
+    D)
+      DIV_OBJ=$OPTARG
+      ;;
+    k)
+      KIN_OBJ=$OPTARG
+      ;;
+    v)
+      VARIANT_ID=$OPTARG
+      ;;
+    s)
+      SAMPLE_ID=$OPTARG
+      ;;
+    n)
+      NUM_CORE=$OPTARG
+      ;;
+    f)
+      FIELD=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+get_controls.R $PHENO_FILE $DX --out_prefix $OUT_PREF
+
+pcair.R $GDS_FILE $DIV_OBJ $KIN_OBJ --variant_id $VARIANT_ID \
+  --sample_id ${OUT_PREF}controls.rds --num_core $NUM_CORE --out_prefix $OUT_PREF
+
+add_fid.R $PLINK_PREF ${OUT_PREF}pcair_unrels.rds --out_file ${OUT_PREF}unrel_controls.txt
+
+plink --recode vcf bgz \
+  --bfile $PLINK_PREF --keep ${OUT_PREF}unrel_controls.txt --out ${OUT_PREF}unrel_controls
+
+ruth_format_pcs.R ${OUT_PREF}pcair_pcs.rds ${OUT_PREF}unrel_controls.txt --out_file ${OUT_PREF}ruth_pcs.txt
+
+ruth --vcf ${OUT_PREF}unrel_controls.vcf.gz --evec ${OUT_PREF}ruth_pcs.txt --out ${OUT_PREF}ruth.vcf \
+  --field $FIELD --site-only
+
+parse_ruth.R ${OUT_PREF}ruth.vcf --out_file ${OUT_PREF}ruth.rds
+
+
