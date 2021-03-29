994f43fec58f0babf977bc59191effd0fe8eb05a
Jai Broome <jb@jaibroome.com>
2021-03-29 15:17:07 -0700

    Fix script order to prepare IDs and PCs for RUTH pipeline

--- get_controls.R
+++ get_controls.R
@@ -5,7 +5,6 @@ library(magrittr)
 argp <- arg_parser("Get IDs of controls") %>%
   add_argument("pheno_file", help="Phenotype file in annotated dataframe format (.rds)") %>%
   add_argument("status", help = "Case/control status variable in pheno_file (binary)") %>%
-  add_argument("plink_prefix", help  = "Prefix for PLINK bed/bim/fam files") %>%
   add_argument("--out_prefix", help = "Prefix for output files",
                default = "") %>%
   add_argument("--sample_id", help = "File with vector of sample IDs to keep (.rds)")
@@ -17,8 +16,6 @@ print(argv)
 library(Biobase)
 library(dplyr)
 
-fam_header <- c("FID", "IID", "FATHER", "MOTHER", "SEX", "PHENOTYPE")
-fam <- paste0(argv$plink_prefix, ".fam") %>% read.table(col.names = fam_header)
 
 pheno <- readRDS(argv$pheno_file) %>% pData()
 controls <- filter(pheno, .data[[argv$status]] == 0)$sample.id
@@ -30,9 +27,5 @@ if (!is.na(argv$sample_id)) {
   keep <- controls
 }
 
-fam_keep <- filter(fam, IID %in% keep) %>% select(FID, IID)
-paste0(argv$out_prefix, "controls.txt") %>%
-  write.table(fam_keep, ., na = "", row.names = FALSE, col.names = FALSE,
-              quote = FALSE)
 
 paste0(argv$out_prefix, "controls.rds") %>% saveRDS(keep, .)
--- ruth_format_pcs.R
+++ ruth_format_pcs.R
@@ -3,7 +3,7 @@ library(argparser)
 library(magrittr)
 
 argp <- arg_parser("Format PC-AiR PCs for RUTH") %>%
-  add_argument("pcair_file", help = "File with pcair object (.rds)") %>%
+  add_argument("pc_file", help = "File with pcair PCs object (.rds)") %>%
   add_argument("fam_file", help = "PLINK .fam file. Can be partial version from get_controls.R") %>%
   add_argument("--out_file", help = "Name for output file",
                default = "ruth_pcs.txt")
@@ -17,8 +17,9 @@ library(dplyr)
 fam <- read.table(argv$fam_file)[,1:2] %>%
   set_colnames(c("FID", "SAMPLE_ID"))
 
-pcair <- readRDS(argv$pcair_file)
-pcs <- as.data.frame(pcair$vectors) %>%
+pcair <- readRDS(argv$pc_file)
+pcs <- readRDS(argv$pc_file) %>%
+  as.data.frame() %>%
   # variable names must be SAMPLE_ID, PC1, PC2, ....
   rename_with(~ gsub("V", "PC", .x)) %>%
   mutate(SAMPLE_ID = rownames(.)) %>%
