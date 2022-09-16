27942398ecc2abd98ac6854dbf78d9d53efe595b
Jai Broome <jb@jaibroome.com>
2022-09-16 15:37:25 -0700

    Add proper handling for when user does not supply genome build for association testing

--- association_test.R
+++ association_test.R
@@ -13,7 +13,6 @@ argp <- arg_parser("Run association test") %>%
   add_argument("--genome_build", help = 'Passed to assocTestSingle. Can be "hg19" or "hg38"') %>%
   add_argument("--dosage", help = "read dosage from DS node", flag = TRUE)
 
-
 argv <- parse_args(argp)
 
 library(SeqArray)
@@ -46,6 +45,11 @@ if (!is.na(argv$sample_id)) {
 } else {
   sample_id <- NULL
 }
+if (is.na(argv$genome_build)) {
+  genome_build <- NULL
+} else {
+  genome_build <- argv$genome_build
+}
 
 gds.id <- seqGetData(gds, "sample.id")
 seqData <- SeqVarData(gds, sampleData = pheno)
@@ -56,7 +60,7 @@ iterator <- SeqVarBlockIterator(seqData, verbose=TRUE)
 nullmod <- readRDS(argv$null_model)
 
 assoc <- assocTestSingle(iterator, nullmod, imputed=argv$dosage,
-                         genome.build = argv$genome_build)
+                         genome.build = genome_build)
 
 if (!is.na(chrom)) {
     out_file <- paste0(argv$out_prefix, "assoc_chr", chrom, ".rds")
