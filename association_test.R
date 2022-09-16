a0dae5339f0c1cbfa9a8c375fd1832df989930de
Jai Broome <jb@jaibroome.com>
2022-09-15 17:06:21 -0700

    Add genome build argument to assocTestSingle

--- association_test.R
+++ association_test.R
@@ -10,6 +10,7 @@ argp <- arg_parser("Run association test") %>%
   add_argument("--sample_id", help = "File with vector of sample IDs") %>%
   add_argument("--chromosome", help = "chromosome number") %>%
   add_argument("--null_model", help = "null model object (.rds)") %>%
+  add_argument("--genome_build", help = 'Passed to assocTestSingle. Can be "hg19" or "hg38"') %>%
   add_argument("--dosage", help = "read dosage from DS node", flag = TRUE)
 
 
@@ -54,7 +55,8 @@ iterator <- SeqVarBlockIterator(seqData, verbose=TRUE)
 
 nullmod <- readRDS(argv$null_model)
 
-assoc <- assocTestSingle(iterator, nullmod, imputed=argv$dosage)
+assoc <- assocTestSingle(iterator, nullmod, imputed=argv$dosage,
+                         genome.build = argv$genome_build)
 
 if (!is.na(chrom)) {
     out_file <- paste0(argv$out_prefix, "assoc_chr", chrom, ".rds")
