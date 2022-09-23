eac69cc558ca0f86949a7d627f410d0299328e42
Jai Broome <jb@jaibroome.com>
2022-09-23 08:48:38 -0700

    Fix parse_ruth to allow for dynamic number of BETA fields

--- parse_ruth.R
+++ parse_ruth.R
@@ -19,10 +19,14 @@ ruth_res <- read.table(argv$ruth_file, sep = "\t", comment.char = "#",
                        col.names = c("CHROM", "POS", "ID", "REF", "ALT", "QUAL",
                                      "FILTER", "INFO")) %>%
   separate(INFO, sep = ";",
-           into = c("PR", "FIBC_P", "HWE_SLP_P", "FIBC_I", "HWE_SLP_I", "MAX_IF",
-                    "MIN_IF", "LLK0", "BETA_IF")) %>%
-  mutate(across(FIBC_P:BETA_IF, ~ sub("^.*=", "", .x))) %>%
-  separate(BETA_IF, paste0("BETA_IF_", 1:5), ",") %>%
-  mutate(across(FIBC_P:BETA_IF_5, as.numeric), ABS_PVAL = abs(HWE_SLP_I))
+           into = c("PR", "FIBC_P", "HWE_SLP_P", "FIBC_I", "HWE_SLP_I",
+                    "MAX_IF", "MIN_IF", "LLK0", "BETA_IF")) %>%
+  mutate(across(FIBC_P:BETA_IF, ~ sub("^.*=", "", .x)))
+# the BETA_IF field will be standard for all observations. Count the number of
+# fields.
+n_sep <- ruth_res$BETA_IF[1] %>% gregexpr(",", .) %>% extract2(1) %>% length()
+out <- separate(ruth_res, BETA_IF, paste0("BETA_IF_", seq(n_sep + 1)), ",") %>%
+  mutate(across(c(FIBC_P:LLK0, matches("BETA_IF")), as.numeric),
+         ABS_PVAL = abs(HWE_SLP_I))
 
-saveRDS(ruth_res, argv$out_file)
+saveRDS(out, argv$out_file)
