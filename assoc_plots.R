e8833a362d5813a5d74e666fb8bee9fcabbe8d31
Jai Broome <jb@jaibroome.com>
2022-12-13 14:41:34 -0800

    add img type for assoc plots

--- assoc_plots.R
+++ assoc_plots.R
@@ -8,6 +8,7 @@ argp <- arg_parser("Association plots") %>%
                type = "numeric", default = NULL) %>%
   add_argument("--maxP_q", help = "Maximum p-value for QQ plot",
                type = "numeric", default = 14) %>%
+  add_argument("--img_ext", help="File extension for plots. Must be one of c('png', 'pdf', jpeg)", default = "png") %>%
   add_argument("--out_prefix", help = "Prefix for output files", default = "")
 argv <- parse_args(argp)
 
@@ -16,16 +17,28 @@ library(fastman)
 sessionInfo()
 print(argv)
 
+img_ext <- argv$img_ext
+
+if (img_ext == "png") {
+  plot_type <- png
+} else if (img_ext == "pdf") {
+  plot_type <- pdf
+} else if (img_ext == "jpeg") {
+  plot_type <- jpeg
+} else {
+  stop("--img_ext must be one of c('png', 'pdf', jpeg)")
+}
+
 assoc <- readRDS(argv$assoc_file)
 
 ## qq plot
-paste0(argv$out_prefix, "qq.png") %>% png(width=7, height=7, units="in", res=300)
+paste0(argv$out_prefix, "qq.", img_ext) %>% plot_type(width=7, height=7, units="in", res=300)
 fastqq(assoc$Score.pval, maxP = argv$maxP_q)
 dev.off()
 
 ## manhattan plot
 assoc <- mutate(assoc, chr = factor(chr, levels = c(0:26,"X","XY","Y")))
 
-paste0(argv$out_prefix, "manh.png") %>% png(width=10, height=6, units="in", res=300)
+paste0(argv$out_prefix, "manh.", img_ext) %>% plot_type(width=10, height=6, units="in", res=300)
 fastman(assoc, "chr", "pos", "Score.pval", maxP = argv$maxP_m)
 dev.off()
